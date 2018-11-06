f3ffdad4d81be153eda50dde828e3d89aa765331
david11yf29 <david11yf29@gmail.com>
2018-11-06 20:34:44 +0800

    Updated

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-    
-  
-    
+$(document).ready(function() {
+  $('.section-features').waypoint(function(direction) {
+        if(direction == "down") {
+          $('nav').addClass('sticky');
+        } else {
+          $('nav').removeClass('sticky');
+        }
+  }, {
+      offset: '60px;'
+  });
 });
